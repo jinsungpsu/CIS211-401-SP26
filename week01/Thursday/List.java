828a528f15bbaaa27523a0703d66fa881c1393dd
Jin An <jin.an@dtcc.edu>
2026-01-15 14:48:02 -0500

    Implement a simple List class with add method

---
+++ week01/Thursday/List.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class List {
+    private int num1;
+    private int num2;
+    private int count = 0;
+    public void add(int number) {
+        if (count == 0) {
+            num1 = number;
+            count++;
+        } else if (count == 1) {
+            num2 = number;
+            count++;
+        } else {
+            // this dumb list is full
+            // can't add anymore
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (count == 0) {
+            return "{}";
+        } else if (count == 1) {
+            return "{" + num1 + "}";
+        } else if (count == 2) {
+            return "{" + num1 + ", " + num2 + "}";
+        }
+        return "";
+    }
+}
+
+class Tester {
+    public static void main(String[] args) {
+        System.out.println("ArrayList example: ");
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(99);
+        System.out.println(list);
+
+        System.out.println("Dumb list example: s");
+        List mylist = new List();
+
+        mylist.add(1);
+        mylist.add(99);
+
+        System.out.println(mylist);
+
+    }
+}
